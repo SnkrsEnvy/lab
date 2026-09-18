78b3be20b836fdc089ee43f715e79e1d79e3e4d1
SnkrsEnvy <snkrsenvy@gmail.com>
2026-09-18 02:09:31 -0500

    CannaCardz #163: prepare clean production share candidate

--- web/api/cannacardz-cover-test.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res) => {
-  const b64 = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/x8AAusB9WlNfGAAAAAASUVORK5CYII=";
-  const buf = Buffer.from(b64, "base64");
-  res.setHeader("Content-Type","image/png");
-  res.setHeader("Content-Length", String(buf.length));
-  res.setHeader("Cache-Control","public, max-age=0, must-revalidate");
-  res.status(200).send(buf);
-};
